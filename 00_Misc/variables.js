9e409e9250d8be6d6fdaf85e0ea4992fc9ddbce0
jaco718v <jaco718v@stud.kea.dk>
2023-08-22 11:10:54 +0200

    Exercises

---
+++ 00_Misc/variables.js
@@ -0,0 +1,18 @@
+// Node - Definition - Enviroment, hvor man kan køre javascript - Kørte tidligere kun i browser
+// Backends - Scripts - Software - CLI
+
+const scheduledBreakTime = "10:30"
+
+//Number instead of int etc.
+
+//Use comma in console log
+console.log("Hello, eee",scheduledBreakTime)
+
+
+//type coercion
+
+// Making Strings
+
+console.log("Double-Quotes")
+console.log('Single-Quotes')
+console.log(`Backticks ${scheduledBreakTime}`)
