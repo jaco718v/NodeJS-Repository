32fd468b38ae2ef0943e35322504110e1f389574
jaco718v <jaco718v@stud.kea.dk>
2023-11-12 22:10:50 +0100

    Used linter+

--- 13_Mandatory2/client/src/pages/signup/signup.svelte
+++ 13_Mandatory2/client/src/pages/signup/signup.svelte
@@ -1,30 +1,37 @@
 <script>
   import { navigate } from "svelte-navigator";
   import { user, BASE_URL } from "../../store/global";
+  import toast, { Toaster } from "svelte-french-toast";
 
   let username;
   let password;
 
   async function signUp() {
-    const response = await fetch($BASE_URL + "/api/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: username.value,
-        password: password.value,
-      }),
-    });
-    const result = await response.json();
-    if (result.data) {
-      user.set(result.data.username);
-      navigate("/");
+    if (username.value && password.value) {
+      const response = await fetch($BASE_URL + "/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username.value,
+          password: password.value,
+        }),
+      });
+      const result = await response.json();
+      if (result.data) {
+        user.set(result.data.username);
+        navigate("/");
+      } else {
+        navigate("/signup");
+      }
     } else {
-      navigate("/signup");
+      toast.error("You are missing either username or password");
     }
   }
 </script>
+
+<Toaster />
 
 <h2>Sign up</h2>
 <label for="username">Username:</label><br />
