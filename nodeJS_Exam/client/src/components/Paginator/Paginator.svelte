b79bde94b48807869f242e295198d201cc11fe8a
jaco718v <jaco718v@stud.kea.dk>
2024-01-12 17:09:24 +0100

    Worked on exam

---
+++ nodeJS_Exam/client/src/components/Paginator/Paginator.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let pagInfo;
+  export let onPressed;
+  const currentPage = pagInfo.currentPage;
+  const numberOfPages = Math.ceil(pagInfo.totalItems / pagInfo.pageLength);
+</script>
+
+<div class="body">
+  {#if currentPage !== 1}
+    <button class="left-arrow" on:click={() => onPressed(-1)}></button>
+  {/if}
+
+  {#if (numberOfPages > 2 && currentPage !== 1) || currentPage === 2}
+    <button class={"square"} on:click={() => onPressed(-1)}
+      >{currentPage - 1}</button
+    >
+  {/if}
+
+  <button class={"squareHL"}>
+    {currentPage}
+  </button>
+
+  {#if numberOfPages > 1 && currentPage !== numberOfPages}
+    <button class={"square"} on:click={() => onPressed(1)}
+      >{currentPage + 1}</button
+    >
+  {/if}
+  {#if currentPage !== numberOfPages}
+    <button class="right-arrow" on:click={() => onPressed(1)}></button>
+  {/if}
+</div>
+
+<style>
+  .body {
+    display: flex;
+    flex-direction: row;
+  }
+  .square {
+    height: 50px;
+    width: 50px;
+    background-color: #555;
+  }
+  .squareHL {
+    height: 50px;
+    width: 50px;
+    background-color: #353ef5;
+  }
+  .left-arrow {
+    width: 0;
+    height: 0;
+    border-top: 25px solid transparent;
+    border-right: 50px solid #555;
+    border-bottom: 25px solid transparent;
+  }
+  .right-arrow {
+    width: 0;
+    height: 0;
+    border-top: 25px solid transparent;
+    border-left: 50px solid #555;
+    border-bottom: 25px solid transparent;
+  }
+</style>
---
+++ nodeJS_Exam/client/src/components/myTable/myTable.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let tableHeaders;
+    export let tableContents;
+    export let componentType;
+    
+    import Book from "../Book/Book.svelte";
+    import Paginator from "../Paginator/Paginator.svelte";
+    
+
+    // function handlePressPaginator(moveNumber){
+    //     pagInfo.currentPage += moveNumber
+    // }
+
+    //<Paginator pagInfo={pagInfo} onPressed={handlePressPaginator}/>
+
+</script>
+
+
+<table class="table" style="max-width:1000px;">
+    <thead>
+      <tr class="bg-secondary" id="theader">
+        {#each tableHeaders as headerName}
+            <th>{headerName}</th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+        {#each tableContents as contentRow}
+            {#if componentType === 'Book'}
+                <tr><Book data={contentRow}/></tr>
+            {/if}
+        {/each}
+    </tbody>
+  </table>
+
+  
