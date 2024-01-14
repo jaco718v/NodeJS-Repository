24d88372bcddb6ed6d000938d3a597f1c0c3fc89
jaco718v <jaco718v@stud.kea.dk>
2024-01-14 17:47:24 +0100

    temp

--- nodeJS_Exam/server/routers/bookRouter.js
+++ nodeJS_Exam/server/routers/bookRouter.js
@@ -3,9 +3,12 @@ import db from "../databases/connection.js";
 
 const router = Router();
 
-router.get("/api/books", async (req,res) => {
-    const query = "SELECT b.book_id, b.title, b.author, b.resume, b.available, (SELECT GROUP_CONCAT(g.genre,', ') from genres g WHERE g.book_id = b.book_id) as genre_list FROM books b;";
-    const sqlData = await db.get(query);
+const pageSize = 2
+
+router.get("/api/books/:page/:sort/:order", async (req,res) => {
+    const Offset = (pageSize * req.params.page) - pageSize
+    const query = "SELECT b.book_id, b.title, b.author, b.resume, b.available, (SELECT GROUP_CONCAT(g.genre,', ') from genres g WHERE g.book_id = b.book_id) as genre_list FROM books b ORDER BY ? ? LIMIT ? OFFSET ?;";
+    const sqlData = await db.all(query,[req.params.sort],[req.params.order],[pageSize], [Offset]);
     res.send({data:{sqlData}})
 })
 
